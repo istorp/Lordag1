9b6c5a83d3bccbd6eb2ddad4ab21044d6341a779
Anders Djerf <37343667+istorp@users.noreply.github.com>
2020-04-13 01:13:06 +0200

    almost done ,, one bug i think

--- Lordag1/Lordag1/Controllers/VehiclesController.cs
+++ Lordag1/Lordag1/Controllers/VehiclesController.cs
@@ -65,5 +65,91 @@ namespace Lordag1.Controllers
             };
             return Ok(vehicles);
         }
+        [HttpGet("[action]")]
+        public IActionResult SearchVehicles(string search)
+        {
+            var vehicles = from v in _cWheelsDbContext.Vehicles
+                           where v.Title.StartsWith(search)
+                           select new
+                           {
+                               Id = v.Id,
+                               Title = v.Title,
+                           };
+            return Ok(vehicles);
+        }
+        [HttpGet]
+        public IActionResult GetVehicles(int categoryId)
+        {
+            var vehicles = from v in _cWheelsDbContext.Vehicles
+                           where v.CategoryId==categoryId
+                           select new
+                           {
+                               Id = v.Id,
+                               Title = v.Title,
+                               Price=v.Price,
+                               Location=v.Location,
+                               DatePosted=v.DatePosted,
+                               IsFeatured=v.IsFeatured,
+                               ImageUrl=v.Images.FirstOrDefault().ImageUrl
+                           };
+            return Ok(vehicles);
+        }
+        [HttpGet("[action]")]
+        public IActionResult VehicleDetails(int id)
+        {
+            var foundVehicle = _cWheelsDbContext.Vehicles.Find(id);
+            if (foundVehicle==null)
+            {
+                return NoContent();
+            }
+
+            var vehicle =  from v in _cWheelsDbContext.Vehicles
+            join u in _cWheelsDbContext.Users on v.UserId equals u.Id
+            where v.Id == id
+            select new
+            {
+                Id = v.Id,
+                Title = v.Title,
+                Description = v.Description,
+                Price = v.Price,
+                Model = v.Model,
+                Engine = v.Engine,
+                Color = v.Color,
+                Company = v.Company,
+                DatePosted = v.DatePosted,
+                Condition = v.Condition,
+                Location = v.Location,
+                Images = v.Images,
+                Email = u.Email,
+                Phone = u.Phone,
+                ImageUrl = u.ImageUrl
+            };
+            return Ok(vehicle);
+        }
+        [HttpGet("[action]")]
+        public IActionResult MyAds()
+        {
+
+            var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email).Value;
+            var user = _cWheelsDbContext.Users.FirstOrDefault(u => u.Email == userEmail);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var vehicles = from v in _cWheelsDbContext.Vehicles
+                           where v.UserId==user.Id
+                           select new
+                           {
+                               Id = v.Id,
+                               Title = v.Title,
+                               Price = v.Price,
+                               Location = v.Location,
+                               DatePosted = v.DatePosted,
+                               IsFeatured = v.IsFeatured,
+                               ImageUrl = v.Images.FirstOrDefault().ImageUrl
+                           };
+            return Ok(vehicles);
+        }
     }
 }
